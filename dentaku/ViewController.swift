0e1d73554dcb295a394c1e7a596d97cc4e63c045
鋤柄里紗 <LisaSukigara@s-risa-sukigara-no-macbook-air.local>
2017-02-05 11:42:05 +0900

    Initial Commit

---
+++ dentaku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dentaku
+//
+//  Created by 鋤柄里紗 on 2017/02/05.
+//  Copyright © 2017年 鋤柄里紗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
